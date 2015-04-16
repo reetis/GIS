1157962920e730723c2143daab61d860a0112e17
Rytis <rytis.stankus@gmail.com>
2015-04-16 16:57:47 +0300

    Cleaned up ZoomToSelectAction code

--- src/main/java/gis/rytis/actions/ZoomToSelectionAction.java
+++ src/main/java/gis/rytis/actions/ZoomToSelectionAction.java
@@ -6,10 +6,7 @@ import org.geotools.map.Layer;
 import org.geotools.swing.MapPane;
 import org.geotools.swing.action.MapAction;
 
-import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class ZoomToSelectionAction extends MapAction {
 
@@ -23,23 +20,15 @@ public class ZoomToSelectionAction extends MapAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        List<ReferencedEnvelope> bounds = getMapPane().getMapContent().layers().stream()
+        ReferencedEnvelope zoomArea = new ReferencedEnvelope();
+
+        getMapPane().getMapContent().layers().stream()
                 .filter(Layer::isSelected)
                 .filter(Layer::isVisible)
                 .filter(x -> x instanceof SelectableLayer)
                 .map(x -> ((SelectableLayer) x).getSelectionBounds())
                 .filter(x -> x != null)
-                .collect(Collectors.toList());
-
-        ReferencedEnvelope zoomArea = null;
-
-        for (ReferencedEnvelope bound: bounds) {
-            if (zoomArea == null) {
-                zoomArea = bound;
-            } else {
-                zoomArea.expandToInclude(bound);
-            }
-        }
+                .forEach(x -> zoomArea.expandToInclude(x));
 
         getMapPane().setDisplayArea(zoomArea);
     }
